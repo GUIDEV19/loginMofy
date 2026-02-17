15269abcea0d4d4f0f6a7280922ac320c3ff073e
Guilherme <guilhermetec358@gmail.com>
2026-02-17 17:50:28 -0300

    estrutura inicial login

---
+++ app/login/page.tsx
@@ -0,0 +1,157 @@
+"use client"
+
+import * as React from "react"
+import { useForm } from "@tanstack/react-form"
+import { toast } from "sonner"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    Field,
+    FieldDescription,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import {
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroupTextarea,
+} from "@/components/ui/input-group"
+import Image from "next/image"
+import Link from "next/link"
+
+const formSchema = z.object({
+    email: z
+        .string()
+        .min(5, "Email must be at least 5 characters.")
+        .max(32, "Email must be at most 32 characters."),
+    password: z
+        .string()
+        .min(8, "Password must be at least 8 characters.")
+        .max(100, "Password must be at most 100 characters."),
+})
+
+export default function Login() {
+    const form = useForm({
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+        validators: {
+            onSubmit: formSchema,
+        },
+        onSubmit: async ({ value }) => {
+            toast("You submitted the following values:", {
+                description: (
+                    <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
+                        <code>{JSON.stringify(value, null, 2)}</code>
+                    </pre>
+                ),
+                position: "bottom-right",
+                classNames: {
+                    content: "flex flex-col gap-2",
+                },
+                style: {
+                    "--border-radius": "calc(var(--radius)  + 4px)",
+                } as React.CSSProperties,
+            })
+        },
+    })
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-background">
+            <Card className="w-full sm:max-w-md mx-auto my-auto border-none shadow-lg bg-card">
+                <Image src="/logo.png" alt="Logo" width={400} height={600} className="mt-[-60px] mx-auto" />
+                <CardHeader className="mt-[-80px]">
+                    <CardTitle className="text-center text-[32px] font-bold">Login Mosfy</CardTitle>
+                    <CardDescription className="text-center text-[12px] font-medium mt-[-10px]">Faça login para continuar</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form
+                        id="login-form"
+                        onSubmit={(e) => {
+                            e.preventDefault()
+                            form.handleSubmit()
+                        }}
+                    >
+                        <FieldGroup>
+                            <form.Field
+                                name="email"
+                                children={(field) => {
+                                    const isInvalid =
+                                        field.state.meta.isTouched && !field.state.meta.isValid
+                                    return (
+                                        <Field data-invalid={isInvalid}>
+                                            <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                                            <Input
+                                                id={field.name}
+                                                name={field.name}
+                                                value={field.state.value}
+                                                onBlur={field.handleBlur}
+                                                onChange={(e) => field.handleChange(e.target.value)}
+                                                aria-invalid={isInvalid}
+                                                placeholder="Digite seu email"
+                                                autoComplete="email"
+                                            />
+                                            {isInvalid && (
+                                                <FieldError errors={field.state.meta.errors} />
+                                            )}
+                                        </Field>
+                                    )
+                                }}
+                            />
+                            <form.Field
+                                name="password"
+                                children={(field) => {
+                                    const isInvalid =
+                                        field.state.meta.isTouched && !field.state.meta.isValid
+                                    return (
+                                        <Field data-invalid={isInvalid}>
+                                            <FieldLabel htmlFor={field.name}>Senha</FieldLabel>
+                                                <Input
+                                                    id={field.name}
+                                                    name={field.name}
+                                                    type="password"
+                                                    value={field.state.value}
+                                                    onBlur={field.handleBlur}
+                                                    onChange={(e) => field.handleChange(e.target.value)}
+                                                    placeholder="Digite sua senha"
+                                                    aria-invalid={isInvalid}
+                                                />
+                                            {isInvalid && (
+                                                <FieldError errors={field.state.meta.errors} />
+                                            )}
+                                        </Field>
+                                    )
+                                }}
+                            />
+                        </FieldGroup>
+                    </form>
+                </CardContent>
+                <CardFooter>
+                    <Field orientation="horizontal">
+                        <div className="flex flex-col w-full">
+                        <Button type="submit" className="text-white font-bold" form="bug-report-form">
+                            Login
+                        </Button>
+                        <p className="text-sm text-gray-500 mt-2 text-center">
+                            Não tem uma conta? <Link href="/register">Cadastre-se</Link>
+                        </p>
+                        </div>
+                    </Field>
+                </CardFooter>
+            </Card>
+        </div>
+    )
+}
